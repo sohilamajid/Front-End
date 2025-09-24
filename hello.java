8179fa48eb805af77ef148e231241f0ce6b1a6ba
sohila <sohilamajid2002@gmail.com>
2025-09-24 11:34:47 +0300

    add new methods

---
+++ hello.java
@@ -0,0 +1,11 @@
+public class Helloworld {
+
+    public void printHelloWorld() {
+        System.out.println("Hello World");
+    }
+
+    public static void main(String[] args) {
+        Helloworld hw = new Helloworld();
+        hw.printHelloWorld();
+    }
+}
---
+++ iti.java
@@ -0,0 +1,11 @@
+public class HelloITI {
+
+    public void printHelloITI() {
+        System.out.println("Hello ITI");
+    }
+
+    public static void main(String[] args) {
+        HelloITI hw = new HelloITI();
+        hw.printHelloITI();
+    }
+}
